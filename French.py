5919d3ac7710b0c5297b0a8497a242419aa6a5bb
Eric042610 <ericc439@nycstudents.net>
2025-02-26 10:14:40 -0500

    commit

---
+++ French.py
@@ -0,0 +1,22 @@
+""" def English_French(text):
+    if text (t and T) > (s and S):
+        print(f"{text} is English.")
+    else:
+        text(f"{text} is French.")
+
+user_text = string(input("Enter text: ")) """
+""" def English_French(text):
+    for i in text:
+        texts = [(t,T) or (s,S)]
+    print(English_French)
+    for i in text:
+        if text (t and T) (s and S):
+            print(f"{text} is English.")
+    else:
+        text(f"{text} is French.")
+        print(i) """
+
+user_text = input("enter a text")
+check for (t,T or s,S)
+for i in user_text:
+    print (i)
